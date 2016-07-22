9b963221f7ef57bfce60e254c28867e49b80d9a6
Alex Efremov <alex.efremov.dev@gmail.com>
2016-07-22 20:56:38 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: [
+    './app/index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
+  },
+  devServer: {
+    inline: true,
+    contentBase: './app',
+    port: 3000
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: [ 'style', 'css', 'sass' ]
+      }
+    ]
+  }
+}
