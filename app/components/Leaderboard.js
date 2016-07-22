c5500a7f933a4e19a3619210b0b869d1170e07df
Alex Efremov <alex.efremov.dev@gmail.com>
2016-07-22 22:05:18 +0300

    Add basic AJAX requests using fetch()

--- app/components/Leaderboard.js
+++ app/components/Leaderboard.js
@@ -3,6 +3,35 @@ import React from 'react';
 import Camper from './Camper';
 
 const Leaderboard = React.createClass({
+  getInitialState: function() {
+    return ({
+        recent: [],
+        alltime: []
+      })
+  },
+  componentDidMount: function() {
+    fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(recent) {
+        console.log(recent);
+      })
+      .catch(function(error) {
+        console.log(error);
+    });
+
+    fetch('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(alltime) {
+        console.log(alltime);
+      })
+      .catch(function(error) {
+        console.log(error);
+    });
+  },
   render: function() {
     return (
       <div className="leaderboard">
