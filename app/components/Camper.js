7e303101f9dba69c36d50f891af6e45f6aee4a2d
Alex Efremov <alex.efremov.dev@gmail.com>
2016-07-24 21:26:57 +0300

    Add links to campers and some styling

--- app/components/Camper.js
+++ app/components/Camper.js
@@ -3,9 +3,12 @@ import React from 'react';
 require('../styles/camper.scss');
 
 const Camper = React.createClass({
+  handleCamperClick: function() {
+    window.open('https://www.freecodecamp.com/' + this.props.camper.username, '_blank');
+  },
   render: function() {
     return (
-      <tr className="camper">
+      <tr className="camper" onClick={this.handleCamperClick}>
         <td className="column-place">{this.props.index}</td>
         <td className="column-camper"><img src={this.props.camper.img} /><span className="camper-username">{this.props.camper.username}</span></td>
         <td className="column-points">{this.props.camper.recent}</td>
